8206d6dba96442299f3762ee69b31c034f4ce8b8
ThisguyNeil <pneilj@gmail.com>
2023-02-16 23:03:09 -0500

    Added questions and answers

---
+++ KnowledgeCheck1/KC1.py
@@ -0,0 +1,20 @@
+#A statement that prints "hello world!"
+print("hello world!")
+
+#A list populated with several values, then, print() one of those values
+best_steak_doneness = ["medium rare", "sometimes rare", "medium if it's a mistake"]
+print(best_steak_doneness)
+
+unforgivable_mistakes = ["medium well", "well done"]
+print(unforgivable_mistakes)
+
+#A dictionary populated with two keys and two values, print one of the values 
+wildly_overrated_movies = {
+    "Romance" : "Titanic",
+    "Horror": "Any Paranormal Activity movies",
+}
+print(wildly_overrated_movies["Horror"])
+
+#A tuple with 4 values, print one of them
+best_halls_flavors = ("Menthol", "Cherry", "Strawberry" "Honey Lemon")
+print(best_halls_flavors[1])
